045c70840b3209b70832168fc8ae2fd98771e3d7
nasreldinkbk09260 <your_email@example.com>
2026-08-07 07:34:52 +0300

    Upload correct files from Alwaha-Pro

--- supabaseClient.js
+++
@@ -1,5 +0,0 @@
-// 1. تهيئة الاتصال بـ Supabase
-const SUPABASE_URL = 'https://YOUR_SUPABASE_PROJECT_URL.supabase.co'; // استبدل برابط مشروعك
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY'; // استبدل بمفتاح ANON الخاص بك
-
-export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
